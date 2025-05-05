5f4683c432a66ed039b129f9ef4d22fbc16953d1
Your Name <youremail@example.com>
2025-05-05 15:10:42 +0800

    feat: wip

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,27 +11,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi recusandae porro quo aliquid iste quod assumenda officiis, unde odit harum veritatis at modi? Cumque harum inventore quaerat et doloribus accusamus ex, culpa dignissimos tempora possimus quo, illum officiis facere id ipsam libero reiciendis eius iusto molestiae nisi. Alias quasi in, reiciendis illum aperiam architecto nostrum. Suscipit consequuntur labore impedit eius aut earum neque quo ab obcaecati distinctio. Pariatur, placeat a itaque provident, perspiciatis labore, recusandae voluptate distinctio deserunt corporis animi commodi? Incidunt et repellendus temporibus quaerat at, omnis numquam accusantium commodi molestias fugit ex sapiente dignissimos expedita possimus, reprehenderit eius.</p>
 </template>
 
 <style scoped>

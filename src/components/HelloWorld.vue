b03b33b28f9cefdcfff931133a60983ec33ee252
Husein8 <huseinsoftic429@example.com>
2025-05-05 15:13:40 +0800

    feat: name

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,6 +12,7 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi recusandae porro quo aliquid iste quod assumenda officiis, unde odit harum veritatis at modi? Cumque harum inventore quaerat et doloribus accusamus ex, culpa dignissimos tempora possimus quo, illum officiis facere id ipsam libero reiciendis eius iusto molestiae nisi. Alias quasi in, reiciendis illum aperiam architecto nostrum. Suscipit consequuntur labore impedit eius aut earum neque quo ab obcaecati distinctio. Pariatur, placeat a itaque provident, perspiciatis labore, recusandae voluptate distinctio deserunt corporis animi commodi? Incidunt et repellendus temporibus quaerat at, omnis numquam accusantium commodi molestias fugit ex sapiente dignissimos expedita possimus, reprehenderit eius.</p>
+  </div>
 </template>
 
 <style scoped>

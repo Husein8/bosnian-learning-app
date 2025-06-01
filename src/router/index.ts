1d52d27460c372b06fb1d08b1fde87960a230882
Husein8 <huseinsoftic429@example.com>
2025-06-01 21:06:19 +0800

    feat: router added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,12 @@
 // src/router/index.ts
-import { createRouter, createWebHistory } from 'vue-router'
-import Homepage from '../views/pages/Homepage.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Homepage from "../views/pages/Homepage.vue";
 
-const routes = [
-  { path: '/', name: 'Home', component: Homepage }
-]
+const routes = [{ path: "/", name: "Home", component: Homepage }];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
